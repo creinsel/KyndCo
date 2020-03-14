deda9c9d01894afcc003bb69ddb888ac78fc976d
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-03-14 12:16:59 -0700

    changed sort

--- controller/KindActsController.js
+++ controller/KindActsController.js
@@ -5,7 +5,7 @@ module.exports = {
   findAll: function(req, res) {
     db.KindActs
       .find(req.query)
-      .sort({ date: -1 })
+      .sort({ tier: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
