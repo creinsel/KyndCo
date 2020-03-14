faf37f5b6322ee7b875a2294e8622b45a32182b3
Mone Duncans-Francis <mone.eunique12@gmail.com>
2020-03-14 13:44:43 -0700

    updating intro compentent

---
+++ client/src/Components/intro/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Bootstrapintro() {
+    return(
+        
+
+        // <!-- ======= About Section ======= -->
+        <div id="about" className="about">
+          <div className="container">
+    
+            <div className="row justify-content-between">
+              <div className="col-lg-5 d-flex align-items-center justify-content-center about-img">
+                <img src="assets/img/about-img.svg" className="img-fluid" alt="" data-aos="zoom-in"/>
+              </div>
+              <div className="col-lg-6 pt-5 pt-lg-0">
+                <h3 data-aos="fade-up">About Us</h3>
+                <p data-aos="fade-up" data-aos-delay="100">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ad quisquam eligendi eaque possimus. Laborum debitis vitae beatae! 
+                  Consectetur harum numquam modi! A, nulla explicabo cumque eos ut aliquid dolorum?
+                </p>
+               
+              </div>
+            </div>
+    
+          </div>
+        </div>
+        
+       
+        
+    );
+}
+
+export default Bootstrapintro;
