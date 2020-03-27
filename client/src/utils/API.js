dd2a5549ecafa86afec1d89043ff9259c5469c32
Cassondra Reinsel <cassiereinsel@gmail.com>
2020-03-26 20:53:51 -0700

    passport

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -31,7 +31,7 @@ export default {
   },
   login: function(formdata) {
     console.log("in the login");
-    return axios.post("/login", formdata );
+    return axios.post("/api/users/login", formdata );
    
   },
   //gets user by id
