f7f4f5272787b67da0c25cc30c251112fd1fd524
Sara Bolduc <sara.s.bolduc@gmail.com>
2020-03-29 11:37:56 -0700

    updates display of date to DOM

--- controller/KindActsController.js
+++ controller/KindActsController.js
@@ -4,7 +4,7 @@ const db = require("../models");
 module.exports = {
   findAll: function(req, res) {
     db.KindAct.find(req.query)
-      .sort({ date: -1 })
+      .sort({ datePerformed: 1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
