1763304495c4d6191f4ac18c4d6c120db0024c09
Mone Duncans-Francis <mone.eunique12@gmail.com>
2020-05-25 14:22:42 -0700

    changing pathway

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Nav from "../../Components/Nav";
-import Intro from "../Components/Intro";
+import Intro from "/Users/moneduncas-francis/Desktop/BootCamp-Homework/KyndCo/client/src/Components/Intro";
 import About from "../../Components/About";
 import Badge from "../../Components/Badge";
 import Team from "../../Components/Team";
